f226d44f533908b7af6ea823ff38cea7b74c0127
Jamey Schaap <75303006+jamey-schaap@users.noreply.github.com>
2024-10-12 18:50:34 +0000

    pointers

--- main.c
+++ main.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
+#include <string.h>
+
+void foo(int *bar);
 
 int main()
 {
-    int x = 4;
-    printf("%d\n", x);
+    // sizeof(char) = 1 byte =  4 bits
+    // sizeof(int)  = 4 byte = 16 bits
+    // a pointer is an int that holds a memory address
+
+    int count = 0;
+    foo(&count);
+    printf("%d\n", count);
+
     return 0;
+}
+
+void foo(int *bar)
+{
+    (*bar)++;
 }
